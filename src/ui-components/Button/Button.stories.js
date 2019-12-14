76b7545dcff66a2c5b4358c477558069b24d30d5
luvuong-le <lu-vuongle@hotmail.com>
2019-12-14 15:16:50 +1100

    Added colours and buttons

---
+++ src/ui-components/Button/Button.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from './Button';
+import SecondaryButton from './SecondaryButton';
+import { styleConstants } from '../../style-constants';
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
+import { css, cx } from 'emotion';
+
+
+export default { title: 'Buttons', decorators: [withKnobs] };
+
+export const Primary = () => (
+    <Button>Primary Button</Button>
+);
+
+export const Secondary = () => (
+    <SecondaryButton>Secondary Button</SecondaryButton>
+);
