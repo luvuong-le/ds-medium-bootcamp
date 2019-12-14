a6134fc330c81dcf1f51df31d2afa97713ef1b15
Tristan Marsh <tristan@connective.com.au>
2019-12-14 15:38:12 +1100

    fix: the breaks

--- src/ui-components/Tile/Tile.stories.js
+++ src/ui-components/Tile/Tile.stories.js
@@ -1,25 +1,23 @@
 import { boolean, number, text, withKnobs } from '@storybook/addon-knobs'
+import { styleConstants, unit } from '../../style-constants'
 
-export default { title: 'Tile', decorators: [withKnobs] };
-
-export const example = () => <div style={{display: 'inline-block', width: unit(13.5)}}>
-    <Tile
-        categoryName={text("Category Name", "Technology")}
-        isPopularTopic="true"
-        title={text("Title", "This is how google will collapse")}
-        subTitle={text("Sub-Title", "Reporting on Googles future")}
-        authorName={text("Author Name", "Tom Kuegler")}
-        authorBlog={text("Author Blog", "Mission.org")}
-        datePublished={text("Date Published", "29/05/2018")}
-        readingTime={text("Reading Time", "6 min read")}
-        isStarred="false">
-    </Tile>
-</div>;
+import React from 'react'
+import Tile from './Tile'
 
 export default { title: 'Tile', decorators: [withKnobs] }
 
 export const example = () => (
   <div style={{ display: 'inline-block', width: unit(13.5) }}>
-    <Tile>{text('Content', 'This is my card Ian!')}</Tile>
+    <Tile
+      categoryName={text('Category Name', 'Technology')}
+      isPopularTopic="true"
+      title={text('Title', 'This is how google will collapse')}
+      subTitle={text('Sub-Title', 'Reporting on Googles future')}
+      authorName={text('Author Name', 'Tom Kuegler')}
+      authorBlog={text('Author Blog', 'Mission.org')}
+      datePublished={text('Date Published', '29/05/2018')}
+      readingTime={text('Reading Time', '6 min read')}
+      isStarred="false"
+    ></Tile>
   </div>
 )
