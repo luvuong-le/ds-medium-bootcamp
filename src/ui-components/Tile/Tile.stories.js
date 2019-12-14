9fc437c220414724be75ca0028a74b2c24c2edeb
Tristan Marsh <tristan@connective.com.au>
2019-12-14 14:43:14 +1100

    chore: enable prettier

--- src/ui-components/Tile/Tile.stories.js
+++ src/ui-components/Tile/Tile.stories.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import Tile from './Tile';
-import { unit } from "../../style-constants";
-import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
+import { boolean, number, text, withKnobs } from '@storybook/addon-knobs'
 
-export default { title: 'Tile', decorators: [withKnobs] };
+import React from 'react'
+import Tile from './Tile'
+import { unit } from '../../style-constants'
 
-export const example = () => <div style={{display: 'inline-block', width: unit(13.5)}}>
-    <Tile>{text("Content", "This is my card Ian!")}</Tile>
-</div>;
+export default { title: 'Tile', decorators: [withKnobs] }
 
-
+export const example = () => (
+  <div style={{ display: 'inline-block', width: unit(13.5) }}>
+    <Tile>{text('Content', 'This is my card Ian!')}</Tile>
+  </div>
+)
